b2c41ce4fc18bbcc74d65a4135fa3034e9648efb
Sarker-Rajib <redhat0593@gmail.com>
2024-08-06 04:11:07 +0600

    build done

--- src/router/Routes/Routes.tsx
+++ src/router/Routes/Routes.tsx
@@ -12,15 +12,15 @@ export const GeneralRouter = createBrowserRouter([
         element: <Home />
     },
     {
-        path: '/product/:productId',
+        path: 'product/:productId',
         element: <ProductDetails />
     },
     {
-        path: '/products',
+        path: 'products',
         element: <AllProducts />
     },
     {
-        path: '/dashboard',
+        path: 'dashboard',
         element: <Dashboard />,
         children: [
             {
